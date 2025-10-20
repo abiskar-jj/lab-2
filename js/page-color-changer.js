350c8d791b7002971c769482e7fd37cfa8059730
abiskar-jj <abiskarchhetri86@gmail.com>
2025-10-19 23:53:51 -0400

    described variables

---
+++ js/page-color-changer.js
@@ -0,0 +1,8 @@
+// COMP1073 - Lab 2: RGB Background Color Changer
+
+// described the variables
+const redSlider = document.getElementById('red');
+const greenSlider = document.getElementById('green');
+const blueSlider = document.getElementById('blue');
+const body = document.body;
+
